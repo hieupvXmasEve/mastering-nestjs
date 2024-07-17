1fd168790860c1ccf9d11312e2f71bf07b58fb90
hieupv <hieupv2412@gmail.com>
2024-07-17 14:51:05 +0700

    add message module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import serverConfig from './config/server.config';
-import databaseConfig, {
-  DatabaseConfig,
-  DatabaseConfigName,
-} from './config/database.config';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import databaseConfig from './config/database.config';
+import { DatabaseConfigService } from './config/database.config.service';
+import { MessageModule } from './message/message.module';
+import serverConfig from './config/server.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [serverConfig, databaseConfig],
       cache: true,
+      isGlobal: true,
     }),
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      connectionName: 'mongo',
-      useFactory: async function (config: ConfigService) {
-        const dbConfig = config.getOrThrow<DatabaseConfig>(DatabaseConfigName);
-        const { user, host, port, name, minPoolSize, maxPoolSize } = dbConfig;
-        const password = encodeURIComponent(dbConfig.password);
-        const uri = `mongodb://${user}:${password}@${host}:${port}/${name}`;
-        return {
-          uri,
-          autoIndex: true,
-          minPoolSize,
-          maxPoolSize,
-          connectTimeoutMS: 60000, // Give up initial connection after 10 seconds
-          socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-        };
-      },
-      inject: [ConfigService],
+      useClass: DatabaseConfigService,
     }),
+    MessageModule,
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ServerConfig, ServerConfigName } from './config/server.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function server() {
   const app = await NestFactory.create(AppModule);
 
+  // Get the server configuration
   const configService = app.get(ConfigService);
   const serverConfig = configService.getOrThrow<ServerConfig>(ServerConfigName);
+
+  // Enable validation globally
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(serverConfig.port);
 }
