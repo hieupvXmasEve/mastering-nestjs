4e5b83f44e7d37661c5db68a772e1718ae3e76c8
hieupv <hieupv2412@gmail.com>
2024-07-17 13:49:59 +0700

    add config and remove file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import serverConfig from './config/server.config';
+import databaseConfig, {
+  DatabaseConfig,
+  DatabaseConfigName,
+} from './config/database.config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [serverConfig, databaseConfig],
+      cache: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      connectionName: 'mongo',
+      useFactory: async function (config: ConfigService) {
+        const dbConfig = config.getOrThrow<DatabaseConfig>(DatabaseConfigName);
+        const { user, host, port, name, minPoolSize, maxPoolSize } = dbConfig;
+        const password = encodeURIComponent(dbConfig.password);
+        const uri = `mongodb://${user}:${password}@${host}:${port}/${name}`;
+        return {
+          uri,
+          autoIndex: true,
+          minPoolSize,
+          maxPoolSize,
+          connectTimeoutMS: 60000, // Give up initial connection after 10 seconds
+          socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
